6cfad5c2f523749af427ef8980c48ec4ba48a31a
Michal <jan766078@gmail.com>
2024-05-28 21:16:44 +0200

    Initial commit

---
+++ src/main/java/com/internet_forum/springboot/InternetForumApplication.java
@@ -0,0 +1,13 @@
+package com.internet_forum.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internet_forum/springboot/InternetForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internet_forum.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
