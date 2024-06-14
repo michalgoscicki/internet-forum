f411434084ea441809c007d6c437dbfdf05c6a4b
NewbieDuck <szymonlupinski1@gmail.com>
2024-06-14 19:57:06 +0200

    [M] jwt token expiry time

--- backendApp/src/main/java/com/internet_forum/springboot/security/JWTGenerator.java
+++ backendApp/src/main/java/com/internet_forum/springboot/security/JWTGenerator.java
@@ -21,7 +21,7 @@ public class JWTGenerator {
 	public String generateToken(Authentication authentication) {
 		String username = authentication.getName();
 		Date currentDate = new Date();
-		Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+		Date expireDate = new Date(currentDate.getTime() + 86400000 + SecurityConstants.JWT_EXPIRATION);
 		
 		String token = Jwts.builder()
 				.setSubject(username)
