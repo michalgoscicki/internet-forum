0371d0e55d6cb57ecc6857d888de93ada984cd42
NewbieDuck <szymonlupinski1@gmail.com>
2024-06-15 23:31:01 +0200

    [M] jwt expire time

--- backendApp/src/main/java/com/internet_forum/springboot/security/SecurityConstants.java
+++ backendApp/src/main/java/com/internet_forum/springboot/security/SecurityConstants.java
@@ -1,5 +1,5 @@
 package com.internet_forum.springboot.security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 86400000;
 }
