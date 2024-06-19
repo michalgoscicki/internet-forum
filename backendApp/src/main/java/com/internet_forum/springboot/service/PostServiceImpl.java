b6954d0ca13d993af76fedc4fc5053c0d83ef02f
NewbieDuck <szymonlupinski1@gmail.com>
2024-06-20 01:41:30 +0200

    [M] Post service update posts by id

--- backendApp/src/main/java/com/internet_forum/springboot/service/PostServiceImpl.java
+++ backendApp/src/main/java/com/internet_forum/springboot/service/PostServiceImpl.java
@@ -1,4 +1,44 @@
 package com.internet_forum.springboot.service;
 
-public class PostServiceImpl {
+import com.internet_forum.springboot.Exceptions.LoggedInUserNotFound;
+import com.internet_forum.springboot.Exceptions.PostNotFoundException;
+import com.internet_forum.springboot.dto.PostRequestDto;
+import com.internet_forum.springboot.dto.PostResponseDto;
+import com.internet_forum.springboot.mapper.PostMapper;
+import com.internet_forum.springboot.model.Post;
+import com.internet_forum.springboot.model.UserEntity;
+import com.internet_forum.springboot.repository.PostRepository;
+import com.internet_forum.springboot.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+@Service
+@AllArgsConstructor
+public class PostServiceImpl implements PostService {
+
+
+    private UserRepository userRepository;
+    private PostRepository postRepository;
+    private PostMapper postMapper;
+
+
+    @Override
+    public PostResponseDto updatePost(Long id, PostRequestDto postRequestDto) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserEntity user = userRepository.findByUsername(userDetails.getUsername())
+                .orElseThrow(() -> new LoggedInUserNotFound());
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException());
+
+
+        post.setContent(postRequestDto.content());
+        post.setModifiedAt(LocalDateTime.now());
+
+        Post updatedPost = postRepository.save(post);
+        return postMapper.entityToResponseDto(updatedPost);
+
+    }
 }
